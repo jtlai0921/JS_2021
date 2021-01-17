52081dcd788dae53e01029abe119983dfe8009f9
不說 <49492695+jtlai0921@users.noreply.github.com>
2021-01-17 14:49:31 +0800

    1

---
+++ 4-2-display-block/flycan.js
@@ -0,0 +1,8 @@
+function openBox() {
+    var n = this.id.substring(6);
+    document.querySelector("#box-" + n).style.display = "block";
+}
+document.querySelector("#title-1").addEventListener("click", openBox);
+document.querySelector("#title-2").addEventListener("click", openBox);
+document.querySelector("#title-3").addEventListener("click", openBox);
+document.querySelector("#title-4").addEventListener("click", openBox);
