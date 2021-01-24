f55573c70a345f6a902619ee903962f3a6a3e115
不說 <49492695+jtlai0921@users.noreply.github.com>
2021-01-24 11:52:37 +0800

    1

--- 6-2-Array-Photo/flycan.js
+++ 6-2-Array-Photo/flycan.js
@@ -1,3 +1,24 @@
+// let n = 0;
+// let word = ["大毛", "二毛", "三毛", "四毛", "五毛", "小毛"];
+// let pic = [
+//     "dog-0.jpg",
+//     "dog-1.jpg",
+//     "dog-2.jpg",
+//     "dog-3.jpg",
+//     "dog-4.jpg",
+//     "dog-5.jpg"
+// ];
+// function nextPhoto() {
+//     if (n < 5) {
+//         n += 1;
+//     } else {
+//         n = 0;
+//     }
+//     document.querySelector("#title").textContent = "我的名字叫" + word[n];
+//     document.querySelector("#pic").src = "images/" + pic[n];
+// }
+// document.querySelector("#box").addEventListener("click", nextPhoto);
+//匿名
 let n = 0;
 let word = ["大毛", "二毛", "三毛", "四毛", "五毛", "小毛"];
 let pic = [
@@ -8,7 +29,8 @@ let pic = [
     "dog-4.jpg",
     "dog-5.jpg"
 ];
-function nextPhoto() {
+
+document.querySelector("#box").addEventListener("click", function () {
     if (n < 5) {
         n += 1;
     } else {
@@ -16,5 +38,4 @@ function nextPhoto() {
     }
     document.querySelector("#title").textContent = "我的名字叫" + word[n];
     document.querySelector("#pic").src = "images/" + pic[n];
-}
-document.querySelector("#box").addEventListener("click", nextPhoto);
+});
